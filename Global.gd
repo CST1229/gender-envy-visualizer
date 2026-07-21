5aed3eab2b124f7264b8e99ef3fca77e01ac7928
CST1229 <68464103+CST1229@users.noreply.github.com>
2026-07-21 08:59:17 +0200

    fullscreen toggle, change warning z-index

--- Global.gd
+++ Global.gd
@@ -1,3 +1,12 @@
 extends Node
 
 var goto_after_download: PackedScene;
+
+func _input(ev: InputEvent) -> void:
+	if ev is InputEventKey && !ev.is_echo() && ev.is_pressed() && (
+		ev as InputEventKey
+	).keycode == KEY_F11:
+		if get_window().mode == Window.MODE_EXCLUSIVE_FULLSCREEN:
+			get_window().mode = Window.MODE_MAXIMIZED;
+		else:
+			get_window().mode = Window.MODE_EXCLUSIVE_FULLSCREEN;
